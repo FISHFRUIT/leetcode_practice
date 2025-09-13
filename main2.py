3066138553c41de99cd1893bbd2f802a1bba7d9c
FISHFRUIT <thaituanhn2106@gmail.com>
2025-09-13 23:19:53 +0700

    feat(main2): add leetcode solution for stock trading and sort vowels - Added call to sortVowels from SortVowelsInAString solution - Added call to maxProfit from BestTimetoBuyandSellStockII - Updated imports to include new Leetcode solution modules

--- main2.py
+++ main2.py
@@ -1,5 +1,5 @@
 from leetcode_problem.hard_problem import powerOfHeroes
-from leetcode_problem.medium_problem import ReverseInteger,SortVowelsInAString
+from leetcode_problem.medium_problem import ReverseInteger,SortVowelsInAString,BestTimetoBuyandSellStockII
 
 # solution = powerOfHeroes.Solution()
 # nums = [1,1,1]
@@ -11,6 +11,11 @@ from leetcode_problem.medium_problem import ReverseInteger,SortVowelsInAString
 # print(s.reverse(x))
 
 # 3
-str = "LEetcOde"
-s = SortVowelsInAString.Solution()
-print(s.sortVowels(str))
+# str = "LEetcOde"
+# s = SortVowelsInAString.Solution()
+# print(s.sortVowels(str))
+
+# 4 
+prices = [7,1,5,3,6,4]
+s = BestTimetoBuyandSellStockII.Solution()
+print(s.maxProfit(prices))
